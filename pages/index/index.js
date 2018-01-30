87c40227d160060d0469f695957c7dee8f129d04
ricegrains <zhangweifeng_nwu@163.com>
2018-01-30 15:50:06 +0800

    update index
    2018-01-30 15：00

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '这是我的第一个小程序',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
